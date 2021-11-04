e3df923f90ae028a611ef60a80968a8767104f3e
ling <971039649@qq.com>
2021-11-04 17:16:02 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    <Test
+      v-for="(item, index) in data"
+      :key="index"
+      :item="item"
+      @remove="remove"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+import { defineComponent, ref } from 'vue'
+import Test from './components/Test.vue'
+export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Test,
+  },
+  setup() {
+    const data = ref([
+      {
+        id: 0,
+        title: '1111',
+      },
+      {
+        id: 1,
+        title: '2222',
+      },
+      {
+        id: 2,
+        title: '3333',
+      },
+      {
+        id: 3,
+        title: '4444',
+      },
+    ])
+    const remove = (id) => {
+      data.value = data.value.filter((item) => item.id !== id)
+    }
+    return {
+      data,
+      remove,
+    }
+  },
+})
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <span>{{ item.title }}</span>
+    <button @click="remove(item.id)">删除</button>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, onBeforeUnmount, onUnmounted } from 'vue'
+
+export default defineComponent({
+  name: 'test',
+  props: {
+    item: Object,
+  },
+  emits: ['remove'],
+  setup(props, context) {
+    const remove = (id: any) => {
+      context.emit('remove', id)
+    }
+    onBeforeUnmount(() => {
+      console.log('ID为' + props.item.id + '的项【准备】被删除')
+    })
+    onUnmounted(() => {
+      console.log('ID为' + props.item.id + '的项【已经】被删除')
+    })
+    return {
+      remove,
+    }
+  },
+})
+</script>
